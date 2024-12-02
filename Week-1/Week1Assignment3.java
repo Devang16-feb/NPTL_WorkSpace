759389816e757a0f3b0260673844bb50c427b622
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-02 20:54:56 +0530

    Update Week1Assignment3.java

--- Week-1/Week1Assignment3.java
+++ Week-1/Week1Assignment3.java
@@ -1,20 +1,21 @@
 
-//Consider First n even numbers starting from zero(0).Complete the code segment to calculate sum of  all the numbers divisible by 3 from 0 to n. Print the sum.
 import java.util.Scanner;
 
-public class Week1Assignment3 {
-    public static void main(String[] args) {
-        // using Scanner Class to take input
+public class Week1Assignment3 
+{
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int sum = 0;
         int result = 1;
         int i = 0;
-        while (result <= n) {
-            // '%' operator is used to find remainder
-            if (i % 2 == 0)// Checking for even number as (evenno.)/2 must have remainder 0
+        
+        while (result <= n) 
+        {
+            if (i % 2 == 0)
             {
-                if (i % 3 == 0)// Checking divisibility by 3 as (number)/3 must have remainder 0
+                if (i % 3 == 0)
                 {
                     sum = sum + i;
 
@@ -23,6 +24,6 @@ public class Week1Assignment3 {
             }
             i = i + 1;
         }
-        System.out.println(sum); // output
+        System.out.println(sum);
     }
 }
