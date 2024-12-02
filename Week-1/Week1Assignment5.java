07f1b515a5848957c1e14e0b931aa24673725874
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-02 20:57:21 +0530

    Update Week1Assignment5.java

--- Week-1/Week1Assignment5.java
+++ Week-1/Week1Assignment5.java
@@ -1,33 +1,31 @@
-
-//Complete the code segment to help Ragav , find the highest mark and average mark secured by him in "s" number of subjects.
 import java.util.Scanner;
 public class Week1Assignment5 {
-    public static void main(String[] args) {
-        // using Scanner Class to take input
+    public static void main(String[] args) 
+    {
         Scanner input = new Scanner(System.in);
         double mark_avg;
         int result;
         int i;
         int s;
-        // define size of array
+        
         s = input.nextInt();
-        // The array is defined "arr" and inserted marks into it.
-        int[] arr = new int[s]; // s is the number of subjects
-        for (i = 0; i < arr.length; i++) {
-            arr[i] = input.nextInt(); // take s number of inputs
+        int[] arr = new int[s]; 
+        for (i = 0; i < arr.length; i++) 
+        {
+            arr[i] = input.nextInt();
         }
         int max = arr[0];
         double sum = arr[0];
-        for (i = 1; i < arr.length; i++) // logic to check highest number and calculating sum
+        
+        for (i = 1; i < arr.length; i++) 
         {
             sum = sum + arr[i];
             if (arr[i] > max)
-                max = arr[i]; // storing highest marks
+                max = arr[i];
         }
-
         result = max;
-        mark_avg = sum / (arr.length); // Average by dividing sum by length
-        System.out.println(result); // Outputs
+        mark_avg = sum / (arr.length);
+        System.out.println(result); 
         System.out.println(mark_avg);
     }
 }
