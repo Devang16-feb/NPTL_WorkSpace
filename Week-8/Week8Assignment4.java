b53ca792ce9b74773c301c25bb0741483593e36e
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-03 22:27:55 +0530

    Update Week8Assignment4.java

--- Week-8/Week8Assignment4.java
+++ Week-8/Week8Assignment4.java
@@ -1,48 +1,42 @@
-/* Write a program to print symmetric Pascal's triangle of "*" 's of  height "l" of odd length . If input "l" is even then your program will print "Invalid line number".
-
-For example:
-
-input : 5
-output:
-  *
- * *
-* * *
- * *
-  *
-input : 6
-
-output:
-
-Invalid line number*/
-
 import java.util.*;
 
-public class Week8Assignment4 {
-  public static void main(String[] args) {
+public class Week8Assignment4 
+{
+  public static void main(String[] args) 
+ {
     Scanner inr = new Scanner(System.in);
     int l = inr.nextInt();
-    // Add the necessary code in the below space
+ 
     int k;
-    if (l % 2 == 0) // To check input is valid or not
+    if (l % 2 == 0) 
       System.out.print("Invalid line number");
-    else {
-      for (int i = 0; i < l; i++) {
+    else 
+    {
+      for (int i = 0; i < l; i++) 
+      {
         k = 0;
-        if (i <= l / 2) {
-          for (int j = l - i; j > l / 2 + 1; j--) {
+        if (i <= l / 2) 
+        {
+          for (int j = l - i; j > l / 2 + 1; j--) 
+          {
             System.out.print(" ");
           }
-          while (k < (i + 1)) {
-            System.out.print("*" + " "); // printing star
+          while (k < (i + 1)) 
+          {
+            System.out.print("*" + " "); 
             k++;
           }
-        } else {
+        } 
+        else 
+        {
           for (int j = l - i; j < l / 2 + 1; j++)
             System.out.print(" ");
-          while (k < (l - i)) { // Printing Star and space
+          while (k < (l - i)) 
+          { 
             if (k == l - i - 1)
               System.out.print("*");
-            else {
+            else 
+            {
               System.out.print("*");
               System.out.print(" ");
             }
@@ -50,7 +44,7 @@ public class Week8Assignment4 {
           }
         }
         if (i != l - 1)
-          System.out.println(); // moving onto next line
+          System.out.println();
       }
     }
   }
