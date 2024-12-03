3d4d9848207919c1469adc67be4521b5080bdc6d
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-03 21:32:52 +0530

    Update Week4Assignment4.java

--- Week-4/Week4Assignment4.java
+++ Week-4/Week4Assignment4.java
@@ -1,31 +1,29 @@
-/* Complete the code segment to call the default method in the interface First and Second */
-
-interface First {
-    // default method
-    default void show() {
+interface First 
+{
+    default void show() 
+    {
         System.out.println("Default method implementation of First interface.");
     }
 }
 
-interface Second {
-    // Default method
-    default void show() {
+interface Second 
+{
+    default void show() 
+    {
         System.out.println("Default method implementation of Second interface.");
     }
 }
 
-// Implementation class code
-class Week4Assignment4 implements First, Second {
-    // Overriding default show method
-    public void show() {
-
-        // Call show() of First interface.
+class Week4Assignment4 implements First, Second 
+{
+    public void show() 
+    {
         First.super.show();
-        // Call show() of Second interface.
         Second.super.show();
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) 
+    {
         Week4Assignment4 q = new Week4Assignment4();
         q.show();
     }
