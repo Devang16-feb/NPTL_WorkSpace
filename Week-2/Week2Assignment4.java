93b402ea733c262dd06b68010a13214bb6ba0e9d
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-03 18:38:42 +0530

    Update Week2Assignment4.java

--- Week-2/Week2Assignment4.java
+++ Week-2/Week2Assignment4.java
@@ -1,22 +1,21 @@
-//Complete the code segment to call default constructor first and then any other constructor in the class.
-// This is the main class Question
-public class Week2Assignment4 {
-    public static void main(String[] args) {
+public class Week2Assignment4 
+{
+    public static void main(String[] args) 
+    {
         Answer a = new Answer(10, "MCQ");
     }
 }
 
-// This is the class Answer
-class Answer {
-    // This is the default constructor of the class Answer
-    Answer() {
+class Answer 
+{
+    Answer() 
+    {
         System.out.println("You got nothing.");
     }
 
-    // This is a parameterized constructor of the class Answer
-    Answer(int marks, String type) {
-        this(); // The this() constructor call can be used to invoke the current class
-                // constructor.
+    Answer(int marks, String type) 
+    {
+        this();
         System.out.print("You got " + marks + " for an " + type);
     }
 }
