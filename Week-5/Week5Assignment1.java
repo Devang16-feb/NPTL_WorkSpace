a9eafab9ddd3571bc37d26c983b6014bfc8fd2b0
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-03 21:42:19 +0530

    Update Week5Assignment1.java

--- Week-5/Week5Assignment1.java
+++ Week-5/Week5Assignment1.java
@@ -1,28 +1,29 @@
-
-//An interface Number is defined in the following program.  You have to declare a class A, which will implement the interface Number. Note that the method findSqr(n) will return the square of the number n.
 import java.util.Scanner;
 
-interface Number {
-    int findSqr(int i); // Returns the square of n
+interface Number 
+{
+    int findSqr(int i); 
 }
 
-// Create a class A which implements the interface Number.
-class A implements Number {
-    // Defining a method to find the square of entered number
+class A implements Number 
+{
     int i, square;
 
-    public int findSqr(int i) {
+    public int findSqr(int i) 
+    {
         square = i * i;
-        return square; // returning output
+        return square; 
     }
 }
 
-public class Week5Assignment1 {
-    public static void main(String[] args) {
-        A a = new A(); // Create an object of class A
-        // Read a number from the keyboard
+public class Week5Assignment1 
+{
+    public static void main(String[] args) 
+    {
+        A a = new A(); 
+        
         Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt(); // input
-        System.out.print(a.findSqr(i)); // output
+        int i = sc.nextInt();
+        System.out.print(a.findSqr(i)); 
     }
 }
