1e98efd52500aafd05588bd5bdaa064801f8f514
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-03 21:46:19 +0530

    Update Week5Assignment5.java

--- Week-5/Week5Assignment5.java
+++ Week-5/Week5Assignment5.java
@@ -1,18 +1,17 @@
-
-/* In the following program, there may be multiple exceptions. You have to complete the code using only one try-catch block to handle all the possible exceptions.
-
-For example, if user�s input is 1, then it will throw and catch �java.lang.NullPointerException�.*/
 import java.util.Scanner;
 
-public class Week5Assignment5 {
-   public static void main(String args[]) {
-      // using Scanner Class to take input
+public class Week5Assignment5 
+{
+   public static void main(String args[]) 
+   {
       Scanner scan = new Scanner(System.in);
       int i = scan.nextInt();
       int j;
-      // Put the following code under try-catch block to handle exceptions
-      try {
-         switch (i) {
+   
+      try 
+      {
+         switch (i) 
+         {
             case 0:
                int zero = 0;
                j = 92 / zero;
@@ -22,12 +21,13 @@ public class Week5Assignment5 {
                j = b[0];
                break;
             default:
-               System.out.print("No exception"); // Default Message
+               System.out.print("No exception"); 
          }
       }
-      // catch block
-      catch (Exception e) {
-         System.out.print(e); // Exception Message
+    
+      catch (Exception e) 
+      {
+         System.out.print(e); 
       }
    }
 }
