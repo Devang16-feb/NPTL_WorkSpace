493ca29b875589011c7a536b7d71a45a97e1fd73
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-03 18:42:43 +0530

    Update Week3Assignment1.java

--- Week-3/Week3Assignment1.java
+++ Week-3/Week3Assignment1.java
@@ -1,26 +1,20 @@
-
-/* This program is related to the generation of Fibonacci numbers.
-
-For example: 0,1, 1,2, 3,5, 8, 13,� is a Fibonacci sequence where 13 is the 8th Fibonacci number. */
 import java.util.Scanner;
 
-public class Week3Assignment1 {
-
-    public static void main(String args[]) {
-        // using Scanner Class to take input
+public class Week3Assignment1 
+{
+    public static void main(String args[]) 
+    {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(fib(n));
     }
 
-    // Template code:
-    static int fib(int n) {
-
-        if (n == 1) // Terminal condition
+    static int fib(int n) 
+    {
+        if (n == 1) 
             return 0;
         else if (n == 2)
             return 1;
-        return fib(n - 1) + fib(n - 2); // Recursive call of function
-
+        return fib(n - 1) + fib(n - 2); 
     }
 }
