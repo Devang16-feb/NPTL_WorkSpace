5723aeabefc776dd8ed7beb0cd1c0616797a5196
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-03 18:37:53 +0530

    Update Week2Assignment3.java

--- Week-2/Week2Assignment3.java
+++ Week-2/Week2Assignment3.java
@@ -1,20 +1,19 @@
-//Complete the code segment to call print() method of class Question by creating a method named �studentMethod()�.
-// This is the main class Question
-public class Week2Assignment3 {
-    public static void main(String[] args) {
-        // Object of the main class is created
+public class Week2Assignment3 
+{
+    public static void main(String[] args) 
+    {
         Week2Assignment3 q = new Week2Assignment3();
-        // Print method on object of Question class is called
         q.studentMethod();
     }
 
-    // 'print()' method is defined in class Question
-    void print(Week2Assignment3 object) {
+    void print(Week2Assignment3 object) 
+    {
         System.out.print("Well Done!");
     }
 
-    public void studentMethod() { // Creating Method
+    public void studentMethod() 
+    { 
         Week2Assignment3 q = new Week2Assignment3();
-        q.print(q); // Calling Print Method
+        q.print(q); 
     }
 }
