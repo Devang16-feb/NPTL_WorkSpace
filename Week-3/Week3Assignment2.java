b47915598254c4db7e21a2a9248623f5da670430
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2024-12-03 18:43:38 +0530

    Update Week3Assignment2.java

--- Week-3/Week3Assignment2.java
+++ Week-3/Week3Assignment2.java
@@ -1,16 +1,12 @@
-/* Define a class Point with two fields x and y each of type double. Also, define a method distance(Point p1, Point p2) to calculate the distance between points p1 and p2 and return the value in double.
-
-Complete the code segment given below. Use Math.sqrt( ) to calculate the square root. */
-
 import java.util.Scanner;
 
-public class Week3Assignment2 extends Point {
-
-    public static void main(String[] args) {
-        // using Scanner Class to take input
-
+public class Week3Assignment2 extends Point 
+{
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         Point p1 = new Point();
+        
         p1.x = sc.nextDouble();
         p1.y = sc.nextDouble();
         Point p2 = new Point();
@@ -18,19 +14,19 @@ public class Week3Assignment2 extends Point {
         p2.y = sc.nextDouble();
         Week3Assignment2 c1 = new Week3Assignment2();
         c1.distance(p1, p2);
-
     }
-
 }
 
-class Point { // defining class Point
+class Point 
+{
     double x;
     double y;
 
-    public static void distance(Point p1, Point p2) {
+    public static void distance(Point p1, Point p2) 
+    {
         double d;
-        d = Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));// Formula to calculate distance
-                                                                                     // betweeen two points
-        System.out.println(d); // output
+        d = Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+                                                                                   
+        System.out.println(d); 
     }
 }
